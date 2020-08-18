8a3ca78194572d0cd00f6c58c7666d9e9396cb17
Sokkay <carmond518@gmail.com>
2020-08-18 08:34:03 -0400

    Added docs folder

---
+++ docs/precache-manifest.648dd9006f4ff0de69f1c186c3601854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e00bb9ffe39c6cad3f67d3ac45b640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0629553c1339c7cf108",
+    "url": "/static/css/main.367eb532.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0629553c1339c7cf108",
+    "url": "/static/js/main.17514deb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
